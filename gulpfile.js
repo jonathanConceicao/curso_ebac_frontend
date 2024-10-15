daa09d3f4a02028dfdc55171badb049017edaa67
jonathanConceicao <145912772+jonathanConceicao@users.noreply.github.com>
2024-10-15 10:38:52 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass') (require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function ComprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('sourcer/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function funcaoPadrao(Callback) {
+    setTimeout(function() {
+    console.log("Executando via Gulp");
+    callback();
+    }, 2000)
+}   
+
+function dizOi(callback) {
+    setTimeout(function() {
+    console.log("Ola Gulp");
+    dizTchau();
+    callback();
+    }, 1000)
+}
+
+function dizTchau() {
+    console.log("Tchau Gulp");
+}
+
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.write = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false} , gulp.series(compilaSass));
+}
+
+exports.javaScript = ComprimeJavaScript;
